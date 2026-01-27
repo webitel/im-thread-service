c5a2549dacae98df92dbc15f84ad9f703832d9ee
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-01-28 00:31:47 +0200

    [WMSG-122]feat(thread): add thread query object support

--- internal/store/query_object/query_object.go
+++ internal/store/query_object/query_object.go
@@ -1,5 +1,162 @@
 package queryobject
 
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/Masterminds/squirrel"
+)
+
 type QueryObject interface {
 	ToSql() (string, []any, error)
+}
+
+type Entity interface {
+	DefaultFields() []string
+	FieldsMetadata() map[string]fieldMetadata
+	EnsureJoins(requiredJoin int)
+}
+
+type fieldMetadata struct {
+	sqlExpr      string
+	aliasedExpr  string
+	requiresJoin int
+	sortable     bool
+	filterExpr   string
+}
+
+type baseQueryObject[T Entity] struct {
+	builder    squirrel.SelectBuilder
+	limit      int
+	fields     []string
+	sortFields []string
+	join       int
+
+	entity T
+}
+
+func newBaseQueryObject[T Entity](from string, ent T) *baseQueryObject[T] {
+	return &baseQueryObject[T]{
+		builder: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar).Select().From(from),
+		limit:   DefaultLimit,
+		join:    0,
+		entity:  ent,
+	}
+}
+
+func (q *baseQueryObject[T]) WithFields(fields []string) T {
+	if len(fields) == 0 {
+		return q.entity
+	}
+
+	validFields := make([]string, 0, len(fields))
+	for _, f := range fields {
+		if meta, ok := q.entity.FieldsMetadata()[f]; ok && !slices.Contains(validFields, f) {
+			validFields = append(validFields, f)
+			q.entity.EnsureJoins(meta.requiresJoin)
+		}
+	}
+
+	q.fields = validFields
+	return q.entity
+}
+
+func (q *baseQueryObject[T]) WithSort(sortFields ...string) T {
+	if len(sortFields) == 0 {
+		return q.entity
+	}
+
+	validSortFields := make([]string, 0, len(sortFields))
+
+	for _, sortField := range sortFields {
+		if len(sortField) < 2 {
+			continue
+		}
+
+		direction := sortField[0:1]
+		fieldName := sortField[1:]
+
+		meta, ok := q.entity.FieldsMetadata()[fieldName]
+		if !ok || !meta.sortable {
+			continue
+		}
+
+		if direction != "+" && direction != "-" {
+			continue
+		}
+
+		q.entity.EnsureJoins(meta.requiresJoin)
+
+		validSortFields = append(validSortFields, sortField)
+	}
+
+	q.sortFields = validSortFields
+	return q.entity
+}
+
+func (q *baseQueryObject[T]) WithLimit(limit int, quota ...int) T {
+	if limit < 0 {
+		limit = DefaultLimit
+	}
+
+	if len(quota) != 0 && limit > quota[0] {
+		limit = quota[0]
+	}
+
+	q.builder = q.builder.Limit(uint64(limit) + 1)
+	return q.entity
+}
+
+func (q *baseQueryObject[T]) WithOffset(page int) T {
+	if page <= 1 {
+		return q.entity
+	}
+
+	offset := (page - 1) * q.limit
+
+	q.builder = q.builder.Offset(uint64(offset))
+
+	return q.entity
+}
+
+func (q *baseQueryObject[T]) escapeLikePattern(s string) string {
+	s = strings.ReplaceAll(s, "\\", "\\\\")
+	s = strings.ReplaceAll(s, "%", "\\%")
+	s = strings.ReplaceAll(s, "_", "\\_")
+	return s
+}
+
+func (q *baseQueryObject[T]) ToSQL() (string, []any, error) {
+	if len(q.fields) < 1 {
+		q.fields = q.entity.DefaultFields()
+	}
+
+	selectExprs := make([]string, 0, len(q.fields))
+	for _, field := range q.fields {
+		if meta, ok := q.entity.FieldsMetadata()[field]; ok {
+			q.entity.EnsureJoins(meta.requiresJoin)
+			selectExprs = append(selectExprs, meta.aliasedExpr)
+		}
+	}
+
+	q.builder = q.builder.Columns(selectExprs...)
+
+	for _, sortField := range q.sortFields {
+		direction := sortField[0:1]
+		fieldName := sortField[1:]
+
+		if meta, ok := q.entity.FieldsMetadata()[fieldName]; ok {
+			orderDir := "ASC"
+			if direction == "-" {
+				orderDir = "DESC"
+			}
+			q.builder = q.builder.OrderBy(fmt.Sprintf("%s %s", meta.sqlExpr, orderDir))
+		}
+	}
+
+	sql, args, err := q.builder.ToSql()
+	sql = CompactSQL(sql)
+
+	return sql, args, err
 }
--- internal/store/query_object/utils.go
+++ internal/store/query_object/utils.go
@@ -1,41 +1,195 @@
 package queryobject
 
 import (
+	"io"
 	"strings"
-
-	"github.com/Masterminds/squirrel"
 )
 
-func joinCTE(ctes []squirrel.Sqlizer) string {
+func CompactSQL(s string) string {
 	var (
-		parts []string
+		r = strings.NewReader(s)
+		w strings.Builder
 	)
 
-	for _, c := range ctes {
-		s, _, _ := c.ToSql()
-		parts = append(parts, s)
-	}
-
-	return strings.Join(parts, ", ")
-}
-
-func formatSort(sort string, allowedFields map[string]bool) (string, bool) {
-	if len(sort) < 2 {
-		return DefaultSortField, true
-	}
+	w.Grow(int(r.Size()))
 
 	var (
-		isDesc = true
-		column = sort[1:]
+		err  error
+		char rune
+		last rune
+		hold rune
+
+		isSpace = func() (is bool) {
+			switch char {
+			case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
+				is = true
+			}
+			return // false
+		}
+		isPunct = func(char rune) (is bool) {
+			switch char {
+			// none; start of text
+			case 0:
+				is = true
+			// special
+			// ':' USES [squirrel] for :named parameters,
+			//     so we need to keep SPACE if there were any
+			case ',', '(', ')', '[', ']', ';', '\'', '"': // , ':':
+				is = true
+			// operators
+			case '+', '-', '*', '/', '<', '>', '=', '~', '!', '@', '#', '%', '^', '&', '|':
+				is = true
+			}
+			return // false
+		}
+		isQuote = func() (is bool) {
+			switch char {
+			case '\'', '"': // SQUOTE, DQUOTE:
+				is = true
+			}
+			return // false
+		}
+		// context
+		space   bool // [IN] [w]hite[sp]ace(s)
+		quote   rune // [IN] [l]i[t]eral(s); *QUOTE(s)
+		comment rune // [IN] [c]o[m]ment; [-|*]
+		// helpers
+		isComment = func() bool {
+			switch comment {
+			case '-':
+				{
+					// comment: close(\n)
+					if char == '\n' { // EOL
+						space = true // inject
+						comment = 0  // close
+						hold = 0     // clear
+					}
+					return true // still IN ...
+				}
+			case '*':
+				{
+					// comment: close(*/)
+					if hold == 0 && char == '*' {
+						// MAY: close(*/)
+						hold = char
+						// need more data ...
+					} else if hold == '*' && char == '/' {
+						space = true // inject
+						comment = 0  // close
+						hold = 0     // clear
+					}
+					return true // still IN ...
+				}
+				// default: 0
+			}
+			// NOTE: (comment == 0)
+			switch hold {
+			// comment: start(--)
+			case '-': // single-line
+				{
+					if char == hold {
+						hold = 0       // clear
+						comment = char // start
+						return true
+					}
+					return false
+				}
+			// comment: start(/*)
+			case '/': // multi-line
+				{
+					if char == '*' {
+						hold = 0       // clear
+						comment = char // start
+						return true
+					}
+					return false
+				}
+			case 0:
+				{
+					// NOTE: (hold == 0)
+					switch char {
+					case '-':
+					case '/':
+					default:
+						// NOT alike ...
+						return false
+					}
+					// need more data ...
+					hold = char
+					// DO NOT write(!)
+					return true
+				}
+			default:
+				{
+					// NO match
+					// need to write hold[ed] char
+					return false
+				}
+			}
+		}
+		isLiteral = func() bool {
+			if !isQuote() || last == '\\' { // ESC(\')
+				return quote > 0 // We are IN ?
+			}
+			// close(?)
+			if quote == char { // inLiteral(?)
+				quote = 0
+				return true // as last
+			}
+			// start(!)
+			quote = char
+			return true
+		}
+		// [re]write
+		output = func() {
+			if hold > 0 {
+				w.WriteRune(hold)
+				last = hold
+				hold = 0
+			}
+			if space {
+				space = false
+				if !isPunct(last) && !isPunct(char) {
+					w.WriteRune(' ') // INJECT SPACE(' ')
+				}
+			}
+			w.WriteRune(char)
+			last = char
+		}
 	)
 
-	if sort[0] == '+' {
-		isDesc = false
+	var e int
+	for {
+
+		char, _, err = r.ReadRune()
+		if err != nil {
+			break
+		}
+		e++ // char index position
+
+		if isComment() {
+			// suppress; DO NOT write(!)
+			continue
+		}
+
+		if isLiteral() {
+			// [re]write: as is (!)
+			output()
+			continue
+		}
+
+		if isSpace() {
+			// fold sequence ...
+			space = true
+			continue
+		}
+		// [re]write: [hold]char
+		output()
 	}
 
-	if !allowedFields[column] {
-		return DefaultSortField, true
+	if err != io.EOF {
+		panic(err)
 	}
 
-	return column, isDesc
+	return w.String()
 }
