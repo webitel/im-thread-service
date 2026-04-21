91b878ecfb3c38527a142549b91c90f3cb85bf87
Dmytro Sumtsov <d.sumtsov@webitel.com>
2026-04-21 11:40:28 +0300

    refactor(messages/system): add check constraint to prevent empty message type

--- migrations/20260415133502_create_system_messages_table.sql
+++ migrations/20260415133502_create_system_messages_table.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS im_message.system_messages;
 
 CREATE TABLE IF NOT EXISTS im_message.system_messages (
   message_id UUID PRIMARY KEY REFERENCES im_message.messages(id) ON DELETE CASCADE,
-  type       VARCHAR(64) NOT NULL,
+  type       VARCHAR(64) NOT NULL CHECK (btrim(type) <> ''),
   metadata   JSONB NOT NULL DEFAULT '{}'::jsonb
 );
 
