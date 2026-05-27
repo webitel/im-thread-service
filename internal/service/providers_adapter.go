95c4f83dfb62f10a70a9a4e47141b738e0fd105c
vhunkaloo <v.hunkalo@webitel.com>
2026-05-27 12:57:55 +0300

    feat(providers_adapter): add detailed logging for external provider message dispatch

--- internal/service/providers_adapter.go
+++ internal/service/providers_adapter.go
@@ -41,10 +41,29 @@ func (a *baseRPCProvidersAdapter) SendMessage(ctx context.Context, message *mode
 		return errors.InvalidArgument("received nil pointer message", errors.WithID("service.providers_adapter.send_message"))
 	}
 
+	log.Debug("evaluating external peers",
+		slog.String("message_id", message.ID.String()),
+		slog.String("thread_id", message.ThreadID.String()),
+		slog.String("message_type", message.Type.String()),
+		slog.Int("total_recipients", len(message.To)),
+	)
+
 	externalPairs := model.ThreadDialogs.ExtractExternalPeers(message.To)
 	if len(externalPairs) == 0 {
+		log.Debug("no external peers found, skipping provider send",
+			slog.String("message_id", message.ID.String()),
+			slog.Int("total_recipients", len(message.To)),
+		)
+
 		return nil
 	}
+
+	log.Info("sending message to external providers",
+		slog.String("message_id", message.ID.String()),
+		slog.String("thread_id", message.ThreadID.String()),
+		slog.String("message_type", message.Type.String()),
+		slog.Int("external_peers_count", len(externalPairs)),
+	)
 
 	var (
 		wg          sync.WaitGroup
@@ -57,9 +76,17 @@ func (a *baseRPCProvidersAdapter) SendMessage(ctx context.Context, message *mode
 			var err error
 
 			userID := externalPeer.ContactID.String()
+			peerLog := log.With(
+				slog.String("external_user_id", userID),
+				slog.String("gate_id", externalPeer.Via),
+				slog.String("message_type", message.Type.String()),
+			)
+
+			peerLog.Debug("dispatching to external provider")
 
 			switch message.Type {
 			case model.MessageTypeFile:
+				peerLog.Debug("sending document", slog.Int("documents_count", len(message.Documents)))
 				_, err = a.providersClient.SendDocument(ctx, &provider.ProviderSendDocumentRequest{
 					GateId:         externalPeer.Via,
 					ExternalUserId: userID,
@@ -68,6 +95,7 @@ func (a *baseRPCProvidersAdapter) SendMessage(ctx context.Context, message *mode
 				})
 
 			case model.MessageTypeImage:
+				peerLog.Debug("sending image", slog.Int("images_count", len(message.Images)))
 				_, err = a.providersClient.SendImage(ctx, &provider.ProviderSendImageRequest{
 					GateId:         externalPeer.Via,
 					ExternalUserId: userID,
@@ -76,37 +104,57 @@ func (a *baseRPCProvidersAdapter) SendMessage(ctx context.Context, message *mode
 				})
 
 			case model.MessageTypeText:
+				peerLog.Debug("sending text")
 				_, err = a.providersClient.SendText(ctx, &provider.ProviderSendTextRequest{
 					GateId:         externalPeer.Via,
 					ExternalUserId: userID,
 					Text:           message.Body,
 				})
-			case model.MessageTypeContact:
-			case model.MessageTypeInteractive:
-			case model.MessageTypeLocation:
-			case model.MessageTypeSystem:
-			case model.MessageTypeUnknown:
 
+			case model.MessageTypeContact:
+				peerLog.Debug("message type contact: not implemented, skipping")
+			case model.MessageTypeInteractive:
+				peerLog.Debug("message type interactive: not implemented, skipping")
+			case model.MessageTypeLocation:
+				peerLog.Debug("message type location: not implemented, skipping")
+			case model.MessageTypeSystem:
+				peerLog.Debug("message type system: not implemented, skipping")
+			case model.MessageTypeUnknown:
+				peerLog.Warn("message type unknown: skipping")
 			}
 
-			if err == nil {
+			if err != nil {
+				peerLog.Error("provider RPC failed",
+					slog.String("error", err.Error()),
+				)
+
+				errorsMu.Lock()
+				errorsArray = append(errorsArray, err)
+				errorsMu.Unlock()
+
 				return
 			}
 
-			errorsMu.Lock()
-
-			errorsArray = append(errorsArray, err)
-			errorsMu.Unlock()
+			peerLog.Debug("provider RPC succeeded")
 		})
 	}
 
 	wg.Wait()
 
 	if len(errorsArray) > 0 {
-		log.Error("sending message to external providers", "error", errorsArray)
+		log.Error("one or more provider sends failed",
+			slog.String("message_id", message.ID.String()),
+			slog.Int("failures", len(errorsArray)),
+			slog.Any("errors", errorsArray),
+		)
 
 		return stderrs.Join(errorsArray...)
 	}
+
+	log.Info("all external providers notified successfully",
+		slog.String("message_id", message.ID.String()),
+		slog.Int("external_peers_count", len(externalPairs)),
+	)
 
 	return nil
 }
