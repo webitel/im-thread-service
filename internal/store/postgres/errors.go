2cedb4d489e76389314055d3c6decc2b3853470e
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-06-10 11:56:42 +0300

    refactor: extend interactive callbacl broker event

--- internal/store/postgres/errors.go
+++ internal/store/postgres/errors.go
@@ -1,16 +1,23 @@
 package postgres
 
 import (
+	"cmp"
+
 	"github.com/jackc/pgx/v5/pgconn"
 	"google.golang.org/grpc/codes"
 
 	"github.com/webitel/webitel-go-kit/pkg/errors"
 )
 
-func uniqueViolation(err error) error {
+func uniqueViolation(err error, errMessage ...string) error {
+	finalErrorMessage := "conflict: unique violation"
+	if len(errMessage) != 0 {
+		finalErrorMessage = cmp.Or(errMessage[0], finalErrorMessage)
+	}
+
 	if pgerr, ok := err.(*pgconn.PgError); ok && pgerr.Code == "23505" {
 		return errors.New(
-			"conflict: unique violation",
+			finalErrorMessage,
 			errors.WithCause(pgerr),
 			errors.WithCode(codes.AlreadyExists),
 		)
